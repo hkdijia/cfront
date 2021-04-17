89d35741a4cdbe797aac7cf7b86742849fc6ba45
huangKai <1181295163@qq.com>
2021-04-17 13:27:36 +0800

    到了 element UI

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
 import { createApp } from 'vue'
+
+import ElementPlus from 'element-plus';
+import 'element-plus/lib/theme-chalk/index.css';
+
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import installElementPlus from './plugins/element'
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App)
+installElementPlus(app)
+app.use(store).use(router).use(ElementPlus).mount('#app')
